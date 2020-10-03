007005a556beca1c64c9e8ec07a14440cc744fa8
Muhammad-Tayyab1 <66192491+Muhammad-Tayyab1@users.noreply.github.com>
2020-10-03 12:56:42 +0500

    addMore

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { Button, Container, Flex, Heading } from 'theme-ui'
 export default props=>(
-    <div>
-        Hello Word
-    </div>
+    <Container>
+        <Flex sx={{flexDirection: "column", margin: 3}}>
+        <Heading as="h1">Get Stuff don</Heading>
+        <Button sx={{marginTop: 2}} onClick={() => {
+            alert('clicked')
+        }}>Log In</Button>
+        </Flex>
+    </Container>
 )
